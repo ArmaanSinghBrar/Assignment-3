2eefb965b217e6cef20badc35d839d44096914c7
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-11 23:04:20 +0530

    Update Question 3.py

--- Question 3.py
+++ Question 3.py
@@ -1,7 +1,7 @@
-#Creating a Tuple with a input number and its square
-number = map(int,input("Enter numbers separated by a space:").split())
-Tuple = []        #an empty list
-for i in number:
-    num2 = (i,i**2)
-    Tuple.append(num2)    #we add the values to emplty list to make a tuple
-print("The tuple with input number and it's square is:\n",Tuple)
+#Printing the input number and its square
+Numbers = map(int,input("Enter numbers separated by a space:").split())
+List=[]                 #Create an empty list
+for i in Numbers:
+    Tupple =(i,i**2)
+    List.append(Tupple)      #Add Tupple to the list
+print("The list of numbers and its square is:\n",List)
