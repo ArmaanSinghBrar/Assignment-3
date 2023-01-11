ba7341376815fdd5eccc08b0cf4d4d0b6ebce3dc
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-11 22:28:48 +0530

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,34 +1,11 @@
-#Word or letter count in string
-string = str(input("Enter the string:"))
-list1 = string.split()
-list_l = []
-for e in list1:
-    lower_e = e.lower()
-    list_l.append(lower_e)
-set1 = set(list_l)
-dic = {}
-for el in set1:
-    count = list_l.count(el)
-    dic.update({el: count})
-list2 = []
-dic2 = {}
-set2 = {1, 2}
-set2.clear()
-if len(list1) == 1:
-
-    for elements in string:
-        list2.append(elements)
-
-    for el in list2:
-        set2.add(el.lower())
-
-    string_l = string.lower()
-    for elem in set2:
-        dic2.update({elem: string_l.count(elem)})
-
-    print("Letter count is :")
-    print(dic2)
-
-else:
-    print("The Word Count is :")
-    print(dic)
+#Number of occurrences of words or alphabets
+List=input("Enter a string").split()
+if len(List)>1:
+    for i in set(List):
+        Count=List.count(i)
+        print(f"Word {i} occurs {Count} times")
+elif len(List)==1:
+    word=List[0]
+    for i in set(word):
+       Count=word.count(i)
+       print(f"Alphabet {i} occurs {Count} times")
