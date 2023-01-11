18f7a0a94f4816aca0534a81c285a47b83339bc6
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-11 22:33:08 +0530

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,11 +1,11 @@
 #Number of occurrences of words or alphabets
 List=input("Enter a string").split()
-if len(List)>1:
-    for i in set(List):
+if len(List)>1:                #if the string is a sentence
+    for i in set(List):        #running a loop for all words in the string
         Count=List.count(i)
         print(f"Word {i} occurs {Count} times")
-elif len(List)==1:
+elif len(List)==1:              #If the string is a word
     word=List[0]
-    for i in set(word):
+    for i in set(word):         #running a loop for all alphabets in the word
        Count=word.count(i)
        print(f"Alphabet {i} occurs {Count} times")
