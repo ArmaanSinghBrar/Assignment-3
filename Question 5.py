b47bd4d5379e2492d69b60a630f1aa5490fa5eab
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-12 14:33:33 +0530

    Update Question 5.py

--- Question 5.py
+++ Question 5.py
@@ -1,8 +1,8 @@
 #Alphabets in reverse pyramid
-a = 6
-for i in range(a):
+Rows = 6
+for i in range(Rows):
     for j in range(i):
-        print(' ', end='')
-    for j in range(2*(a-i)-1):
-        print(chr(65 + j), end='')
+        print(" ", end="")
+    for j in range(2*(Rows-i)-1):
+        print(chr(65 + j), end="")
     print()
